70d1a6427f6c97f0a677a599b690edde2170d518
Sabrina <sabrinama10@gmail.com>
2017-04-20 08:41:53 -0400

    made GameActivity

---
+++ app/src/main/java/com/example/sabrinama/chineseflashcards/GameActivity.java
@@ -0,0 +1,20 @@
+package com.example.sabrinama.chineseflashcards;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        String message= intent.getStringExtra("radioChosen");
+        TextView textview = (TextView) findViewById(R.id.textView);
+        textview.setText(message);
+    }
+}
--- app/src/main/java/com/example/sabrinama/chineseflashcards/StartActivity.java
+++ app/src/main/java/com/example/sabrinama/chineseflashcards/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.sabrinama.chineseflashcards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        String buttonId = "";
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.english_button:
+                if (checked)
+                    buttonId = "english_button";
+                break;
+            case R.id.pinyin_button:
+                if (checked)
+                    buttonId = "pinyin_button";
+                break;
+            case R.id.chinese_button:
+                if (checked)
+                    buttonId = "chinese_button";
+                break;
+        }
+
+        //Send selected radio button id to GameActivity
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("radioChosen", buttonId);
+        startActivity(intent);
     }
 }
